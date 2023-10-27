df00aa2fe6ac931ceb4c6eb2c54157b9d903154c
zip-fa <61551308+zip-fa@users.noreply.github.com>
2023-10-27 10:37:12 +0300

    bug repro

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, ErrorHandler } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { createErrorHandler, TraceService } from '@sentry/angular-ivy';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    {
+      provide: ErrorHandler,
+      useValue: createErrorHandler({
+        showDialog: false,
+        logErrors: true
+      })
+    },
+  ]
 };
